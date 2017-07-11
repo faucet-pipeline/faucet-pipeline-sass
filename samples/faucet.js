3045f959ff95f1428cb72daf7f09e01e9a2da16c
Lucas Dohmen <lucas@dohmen.io>
2017-07-11 20:19:51 +0200

    Rename examples to samples and add them to the `node t` style check
    Another adjustment to make the faucet-pipeline-* repos more similar

---
+++ samples/faucet.js
@@ -0,0 +1,26 @@
+module.exports = {
+	css: {
+		targetDir: "./output",
+		manifest: {
+			file: "./css.json",
+			baseURI: "/assets"
+		},
+		assets: [
+			"./images.json"
+		],
+		prefixes: {
+			browsers: [
+				"last 2 versions",
+				"Chrome 21"
+			]
+		},
+		bundles: [
+			{
+				entryPoint: "./example.scss"
+			},
+			{
+				entryPoint: "./example2.scss"
+			}
+		]
+	}
+};
