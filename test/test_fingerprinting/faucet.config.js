b7fae40146aa7d9f3bb66ccc036937dfcde1416f
Lucas Dohmen <lucas@dohmen.io>
2018-04-06 09:26:06 +0200

    Support per-bundle fingerprint configuration

--- test/test_fingerprinting/faucet.config.js
+++ test/test_fingerprinting/faucet.config.js
@@ -4,7 +4,11 @@ let path = require("path");
 module.exports = {
 	sass: [{
 		source: "./src/index.scss",
-		target: "./dist/bundle.css"
+		target: "./dist/fingerprint/bundle.css"
+	}, {
+		source: "./src/index.scss",
+		target: "./dist/no-fingerprint/bundle.css",
+		fingerprint: false
 	}],
 	manifest: {
 		target: "./dist/manifest.json",
